29f67b022cf518d3ab1ce0b18fd142e595f77642
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2023-07-23 09:50:08 +0200

    Push Update 23/07/2023, 09:50:08

--- src/main.c
+++ src/main.c
@@ -22,6 +22,7 @@ int main(void){
 	srand(seed);
 	allocator_t allocator;
 	allocator_init((1<<ALLOCATOR_SIZE_SHIFT)-1,&allocator);
+	printf("Allocator span: %u/%u -> %.3f%%\n",allocator.size,TEST_SIZE,((float)(allocator.size))/TEST_SIZE*100);
 	// Random access
 	unsigned int cache_miss_count=0;
 	for (unsigned int i=0;i<TEST_SIZE;i++){
@@ -31,18 +32,24 @@ int main(void){
 			cache_miss_count++;
 		}
 	}
-	printf("%u/%u -> %.2f\n",cache_miss_count,TEST_SIZE,((float)cache_miss_count)/TEST_SIZE);
-	// Localized access
+	printf("Random access cache misses: %u/%u -> %.3f%%\n",cache_miss_count,TEST_SIZE,((float)cache_miss_count)/TEST_SIZE*100);
+	// Local access
 	cache_miss_count=0;
-	unsigned int j=TEST_SIZE>>1;
+	int j=TEST_SIZE>>1;
 	for (unsigned int i=0;i<TEST_SIZE;i++){
 		if (!allocator_get(&allocator,j,NULL)){
 			allocator_set(&allocator,j,(void*)1);
 			cache_miss_count++;
 		}
-		j+=_get_random_number(-4,4);
+		j+=_get_random_number(-2,2);
+		if (j<0){
+			j=0;
+		}
+		else if (j>=TEST_SIZE){
+			j=TEST_SIZE-1;
+		}
 	}
-	printf("%u/%u -> %.2f\n",cache_miss_count,TEST_SIZE,((float)cache_miss_count)/TEST_SIZE);
+	printf("Local access cache misses: %u/%u -> %.3f%%\n",cache_miss_count,TEST_SIZE,((float)cache_miss_count)/TEST_SIZE*100);
 	allocator_deinit(&allocator);
 	return 0;
 }
